55ff86a6ca286e4b68862a5c719344dd23b74f1b
mariem184 <mar780032@gmail.com>
2026-07-07 02:21:47 +0300

    save changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { NotificationService } from './core/Services/notification.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('FinalProject');
-
-  constructor(public notificationService: NotificationService) {}
 }
--- src/app/features/Layout/admin-layout/admin-layout.ts
+++ src/app/features/Layout/admin-layout/admin-layout.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Header } from '../../../shared/Components/header/header';
 import { Sidebar } from '../../../shared/Components/sidebar/sidebar';
 import { AuthService } from '../../../core/Services/auth';
+import { NotificationService } from '../../../core/Services/notification.service';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet,Header,Sidebar],
+  imports: [RouterOutlet, Header, Sidebar, CommonModule],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.css'
 })
 export class AdminLayout implements OnInit {
   isSidebarOpen = false;
 
-  constructor(private _AuthService: AuthService, private _Router: Router) {}
+  constructor(
+    private _AuthService: AuthService, 
+    private _Router: Router,
+    public notificationService: NotificationService
+  ) {}
 
   ngOnInit() {
     if (!this._AuthService.userData.value || !this._AuthService.isAdmin()) {
